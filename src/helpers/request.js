a7a7119d286b49a3bba87be1416e5fd4db1e7151
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-25 03:47:26 +0200

    feat(request): добавлена функция для создания запросов

---
+++ src/helpers/request.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const axios = require('axios');
+const getCookie = require('./getCookie');
+
+module.exports = async (endpoint, { method = 'get', body } = {}) => {
+  console.log(await getCookie());
+  const res = await axios[method](`${process.env.FORUM_URL}${endpoint}`, {
+    headers: {
+      Cookie: `R3ACTLB=${await getCookie()}`,
+    },
+    body: body ? JSON.stringify(body) : undefined,
+    method,
+  });
+  const isSuccess = res && res.status === 200;
+  return isSuccess ? res.data : null;
+};
